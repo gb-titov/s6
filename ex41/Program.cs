6e3bf036d13b6e8b23391ea6d05a7ceba15ca833
Yury Titov <yuriy-t87@yandex.ru>
2022-08-10 10:53:09 +0300

    Добавьте файлы проекта.

---
+++ ex41/Program.cs
@@ -0,0 +1,64 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//-1, -7, 567, 89, 223-> 3
+
+
+Console.Clear();
+Console.WriteLine("Программа для подсчета количества натуральных чисел.");
+
+int[] array = GetArrayFromUserInput();
+
+int naturalNumsCount = CountNaturalNums(array);
+
+Console.WriteLine($"{WriteArrayAsString(array)} -> {naturalNumsCount}");
+
+
+#region Methods
+
+int CountNaturalNums(int[] arr) => arr.Count(i => i > 0);
+
+string WriteArrayAsString(int[] arr) => $"[{string.Join(", ", arr)}]";
+
+int[] GetArrayFromUserInput()
+{
+    while (true)
+    {
+        Console.Write("Введите несколько чисел через запятую: ");
+        var userInputStr = Console.ReadLine();
+
+        int[] arr = GetArray(userInputStr);
+        if (arr != null) return arr;
+
+        Console.WriteLine("Неверный ввод! Попробуйте снова.");
+    }
+
+
+}
+
+int[] GetArray(string? str)
+{
+    if (string.IsNullOrEmpty(str) || !str.Contains(','))
+    {
+        return null;
+    }
+
+    var arr = str.Split(",");
+
+    var intArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (!int.TryParse(arr[i], out int r))
+        {
+            return null;
+        }
+
+        intArr[i] = r;
+    }
+
+    return intArr;
+}
+
+#endregion
+
---
+++ ex43/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// 
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+using ex43;
+
+
+Console.Clear();
+Console.WriteLine("Программа для нахождения точки пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2");
+
+double b1 = GetUserInputAsDouble("Введите b1");
+double k1 = GetUserInputAsDouble("Введите k1");
+double b2 = GetUserInputAsDouble("Введите b2");
+double k2 = GetUserInputAsDouble("Введите k2");
+
+
+Point point = FindIntersection(k1, b1, k2, b2);
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> {point} ");
+
+
+
+#region Methods
+
+Point FindIntersection(double k1, double b1, double k2, double b2)
+{
+    //k1*x+b1 - (k2*x+b2) = (k1-k2)x + b1 - b2 = (k1-k2)x = -b1 + b2 = (-b1 + b2) / (k1 - k2)
+    double x = (-b1 + b2) / (k1 - k2);
+    double y = k1 * x + b1;
+    return new Point(x, y);
+}
+
+double GetUserInputAsDouble(string msg)
+{
+    Console.Write($"{msg}: ");
+    if (!double.TryParse(Console.ReadLine(), out double result))
+    {
+        Console.WriteLine("Неверный ввод! Попробуйте снова.");
+        GetUserInputAsDouble(msg);
+    }
+    return result;
+}
+
+#endregion
